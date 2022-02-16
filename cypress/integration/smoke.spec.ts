afba2288dc2f8ffa651a24f4778e029d5ed32081
Matthew Burfield <m1.burfield@gmail.com>
2022-02-16 21:08:22 +1000

    chore: eslint and prettier works

--- cypress/integration/smoke.spec.ts
+++ cypress/integration/smoke.spec.ts
@@ -1,6 +1,6 @@
-describe('Smoke test', () => {
-  it('should view the home page', () => {
-    cy.visit('/');
-    cy.contains('Welcome to Remix');
-  })
-})
+describe("Smoke test", () => {
+  it("should view the home page", () => {
+    cy.visit("/");
+    cy.contains("Welcome to Remix");
+  });
+});
